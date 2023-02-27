1ce6b3aeff4dad2891ae8dd3ce3b2e03ff012f9a
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-02-28 02:17:54 +0530

    Setup devise authentication

--- Gemfile
+++ Gemfile
@@ -64,3 +64,7 @@ end
 gem "webpacker"
 
 gem "react-rails"
+
+gem "devise"
+
+gem "devise_token_auth"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for "User", at: "users"
   root "home#index"
   get "*path", to: "home#index", via: :all
 end
