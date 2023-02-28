0c2c18a95892862251780c3e57eda97c1c7c3d0d
Devu R Kurup <40559138+devukurup@users.noreply.github.com>
2023-02-28 13:01:03 +0530

    Added a resource class method

--- app/controllers/concerns/invitable_methods.rb
+++ app/controllers/concerns/invitable_methods.rb
@@ -3,6 +3,15 @@
 module InvitableMethods
   extend ActiveSupport::Concern
 
+  def resource_class(m = nil)
+    if m
+      mapping = Devise.mappings[m]
+    else
+      mapping = Devise.mappings[resource_name] || Devise.mappings.values.first
+    end
+    mapping.to
+  end
+
   def resource_from_invitation_token
     @user = User.find_by_invitation_token(params[:invitation_token], true)
     return if params[:invitation_token] && @user
